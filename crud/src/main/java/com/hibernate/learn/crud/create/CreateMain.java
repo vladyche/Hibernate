4840171b921bd4cf7a26d35a31bb31fead0d5fe1
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-10-27 00:29:52 +0300

    add session factory, session, build object and save object

--- crud/src/main/java/com/hibernate/learn/crud/create/CreateMain.java
+++ crud/src/main/java/com/hibernate/learn/crud/create/CreateMain.java
@@ -1,20 +1,43 @@
 package com.hibernate.learn.crud.create;
 
+import com.hibernate.learn.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class CreateMain {
     public static void main(String[] args){
         //session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+
         //session
+        Session session = sessionFactory.getCurrentSession();
+
         //prepare object for save
+        User user = new User();
+        user.setFirstName("Anakin");
+        user.setLastName("Skywalker");
 
         try{
             //start transaction
+            session.beginTransaction();
+
             //save object
+            session.save(user);
+
             //commit the transaction
+            session.getTransaction().commit();
         }catch (Exception e){
             e.printStackTrace();
         }finally {
             //close session
+            session.close();
+
             //close factory session
+            sessionFactory.close();
         }
     }
 }
