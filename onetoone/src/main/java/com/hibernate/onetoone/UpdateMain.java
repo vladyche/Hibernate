deedb42ec78c961eb12635ad3e00f5bb193c19d5
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-10-31 00:39:39 +0300

    one to one relationship UPDATE

--- onetoone/src/main/java/com/hibernate/onetoone/UpdateMain.java
+++ onetoone/src/main/java/com/hibernate/onetoone/UpdateMain.java
@@ -1,7 +1,44 @@
 package com.hibernate.onetoone;
 
+import com.hibernate.onetoone.entity.User;
+import com.hibernate.onetoone.entity.UserLink;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class UpdateMain {
     public static void main(String[] args) {
+        //session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(UserLink.class)
+                .buildSessionFactory();
 
+        //session
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+            //start session
+            session.beginTransaction();
+
+            //update user with id=1
+            User user = session.get(User.class, 1L);
+            user.setFirstName("NewFirstName");
+            user.setLastName("NewLastName");
+
+            //update youtube link for user with id=1
+            UserLink userLink = user.getUserLink();
+            userLink.setYouTube("youtube.com/new");
+
+            //commit session
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            //close session and session factory
+            session.close();
+            sessionFactory.close();
+        }
     }
 }
