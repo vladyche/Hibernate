26b142b3f4e29da24c87d8f74431dc11063e6dea
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-10-31 01:44:28 +0300

    one to one relationship DELETE

--- onetoone/src/main/java/com/hibernate/onetoone/DeleteMain.java
+++ onetoone/src/main/java/com/hibernate/onetoone/DeleteMain.java
@@ -1,7 +1,37 @@
 package com.hibernate.onetoone;
 
+import com.hibernate.onetoone.entity.User;
+import com.hibernate.onetoone.entity.UserLink;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class DeleteMain {
     public static void main(String[] args){
+        //session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(UserLink.class)
+                .buildSessionFactory();
+
+        //session
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+            session.beginTransaction();
+
+            //delete record with id=1
+            User user = session.get(User.class, 1L);
+            session.delete(user);
+
+            session.getTransaction().commit();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            session.close();
+            sessionFactory.close();
+        }
 
     }
 }
