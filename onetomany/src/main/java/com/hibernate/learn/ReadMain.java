6ff82a2ecd1f1771cad37e0d850e6f95cc60b763
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-11-05 00:45:29 +0200

    one to many READ

--- onetomany/src/main/java/com/hibernate/learn/ReadMain.java
+++ onetomany/src/main/java/com/hibernate/learn/ReadMain.java
@@ -1,4 +1,42 @@
 package com.hibernate.learn;
 
+import com.hibernate.learn.entity.Link;
+import com.hibernate.learn.entity.Menu;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class ReadMain {
+    public static void main(String[] args){
+        //session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Menu.class)
+                .addAnnotatedClass(Link.class)
+                .buildSessionFactory();
+
+        //session
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+            //begin transaction
+            session.beginTransaction();
+
+            //read links for menu with id=1
+            Menu menu = session.get(Menu.class, 1L);
+            System.out.println(menu.getLinks());
+
+            //read menu with id=2
+            Menu menu2 = session.get(Menu.class, 2L);
+            System.out.println(menu2);
+
+            //commit the transaction
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
 }
--- onetomany/src/main/resources/data.sql
+++ onetomany/src/main/resources/data.sql
@@ -2,3 +2,5 @@ insert into menu (id, name) values (1, 'Category Menu');
 insert into menu (id, name) values (2, 'Main Menu');
 
 insert into link (href, text, menu_id) values ('reviews', 'Reviews', 1);
+insert into link (href, text, menu_id) values ('projects', 'Projects', 1);
+insert into link (href, text, menu_id) values ('software', 'Software', 1);
